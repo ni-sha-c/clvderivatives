60c7156f81ae90aa1b5aba0594cbe5b6dacd4f47
Nisha Chandramoorthy <nishac@mit.edu>
2020-05-06 10:18:41 -0400

    add test to ensure clvs are correct for Cat map

--- test_cat.jl
+++ test_cat.jl
@@ -1,7 +1,17 @@
 include("cat.jl")
+include("clvs.jl")
 using Test
 u = rand(100,2)
 s = rand(2)
-@test isapprox(dCat_fd(u,s), dCat(u,s), rtol=1.e-6)
+@test isapprox(dCat_fd(u,s), dCat(u,s), rtol=1.e-2)
+function test_clvs()
+    u = rand(1,2)
+	s = [rand(),0.2]
+    u_trj = Cat(u, s, 20000)[:,:,1]
+    du_trj = dCat(u_trj, s)
+    les, clv_trj = clvs(du_trj, 2) 
+    return les
+end
+@test isapprox(test_clvs(), [0.9,-0.9],rtol=0.1)
 
 
